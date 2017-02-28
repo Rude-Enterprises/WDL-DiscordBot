0cc151b03f8947885330aed43fdbfe8db59c9ae3
jwolfe <jgwolfe@udel.edu>
2017-02-28 17:21:54 -0500

    took username/pass out ...

--- WDLBOTREAL.py
+++ WDLBOTREAL.py
@@ -244,4 +244,4 @@ if __name__ == '__main__':
     for extension in initial_extensions:
         bot.load_extension(extension)  # This adds the cogs listed in initial_extensions to the bot
 
-    bot.run("wolfeman312@gmail.com", "itchyshorts99")
+    bot.run("username", "pass")
