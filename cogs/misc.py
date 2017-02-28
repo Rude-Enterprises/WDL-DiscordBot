55cf94ac39338dafbd64b99a53286459f5d9fa63
jwolfe <jgwolfe@udel.edu>
2017-02-28 17:04:57 -0500

    Organization of commands

---
+++ cogs/misc.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+
+class misc:
+    def __init__(self, bot):  # This allows the cog to access the bot, and its functions
+        self.bot = bot
+
+    @commands.command()  # in cogs you use @commands.command instead of @bot.command
+    async def more(self):  # commands have to be inside the cog class
+        await self.bot.say("More cool stats can be found here: http://doomleague.org/forums/index.php?board=8.0")  # Use self.bot inside cogs
+
+    @commands.command()
+    async def help(self):
+        help_text = """Hello I am a bot. beep boop
+                                  \nCheck my FAQ: https://github.com/jesswolfe/WDL-DiscordBot/blob/master/commands.md"""
+        await self.bot.say(help_text)
+
+
+def setup(bot):
+    bot.add_cog(misc(bot))
