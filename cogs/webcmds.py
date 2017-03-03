9c5c423f4c1f7269029660538c9ea3580fd7a200
jwolfe <jgwolfe@udel.edu>
2017-03-03 16:06:58 -0500

    added !gametime command, user command that does the same thing as gametime_checker

--- cogs/webcmds.py
+++ cogs/webcmds.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import bs4 as bs
 import urllib.request
+from datetime import datetime
 import re
 import pandas as pd
 
@@ -20,7 +21,28 @@ class Web():
         sauce = urllib.request.urlopen("http://doomleague.org/").read()
         soup = bs.BeautifulSoup(sauce, "lxml")
         game_times = soup.find_all(text=re.compile(rege_str))
+        for any in game_times:
+            date_objects = []
+            date_objects.append(datetime.datetime.strptime(any, "Gametime: %A, %b %d @ %I:%M%p %Z"))
+
         await self.bot.say(game_times[:4])
+
+    @commands.command()
+    async def gametime(self):
+        rege_str = r"Gametime:\s[\w]+,\s[\w]{3}\s[0-9]+\s@\s[0-9]+:[0-9][0-9]PM\sEST"
+        sauce = urllib.request.urlopen("http://doomleague.org/").read()
+        soup = bs.BeautifulSoup(sauce, "lxml")
+        game_times = soup.find_all(text=re.compile(rege_str))
+        date_objects = []
+        tday = datetime.today()
+
+        for any in game_times:
+            date_objects.append(datetime.strptime(any, "Gametime: %A, %b %d @ %I:%M%p EST"))
+        for any in date_objects:
+            if any.day == tday.day and any.month == tday.month:
+                await self.bot.say("game on the {}".format(tday.day))
+            else:
+                pass
 
 def setup(bot):
     bot.add_cog(Web(bot))
