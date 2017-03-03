a6aade38de6e6c66e8f52074f6a1bec3abdb5963
jwolfe <jgwolfe@udel.edu>
2017-03-03 16:29:03 -0500

    !today is now user command of gametime_checker()

--- cogs/webcmds.py
+++ cogs/webcmds.py
@@ -43,6 +43,56 @@ class Web():
                 await self.bot.say("game on the {}".format(tday.day))
             else:
                 pass
+    @commands.command()
+    async def today(self):
+        gametime_str = r"Gametime:\s[\w]+,\s[\w]{3}\s[0-9]+\s@\s[0-9]+:[0-9][0-9]PM\sEST"
+        team_str = r"([\w]+)+\s\[...\]"
+        sauce = urllib.request.urlopen("http://doomleague.org/").read()
+        soup = bs.BeautifulSoup(sauce, "lxml")
+        game_times = soup.find_all(text=re.compile(gametime_str))
+        matchups = soup.find_all(text=re.compile(team_str))
+        date_objects = []
+        tday = datetime.today()
+
+        for any in game_times:
+            date_objects.append(datetime.strptime(any, "Gametime: %A, %b %d @ %I:%M%p EST"))
+        for any in date_objects:
+            if any.day == tday.day and any.month == tday.month:
+                if any == date_objects[0]:
+                    await self.bot.say("{} vs {} today, {}/{}- at {}:{} EST!".format(matchups[0], matchups[1],
+                                                                        any.month, any.day, any.hour, any.minute))
+                elif any == date_objects[1]:
+                    await self.bot.say(
+                                           "{} vs {} today, {}/{}- at {}:{} EST!".format(matchups[2], matchups[3],
+                                                                        any.month, any.day, any.hour, any.minute))
+                elif any == date_objects[2]:
+                    await self.bot.say(
+                                           "{} vs {} today, {}/{}- at {}:{} EST!".format(matchups[4], matchups[5],
+                                                                        any.month, any.day, any.hour, any.minute))
+                elif any == date_objects[3]:
+                    await self.bot.say(
+                                           "{} vs {} today, {}/{}- at {}:{} EST!".format(matchups[6], matchups[7],
+                                                                        any.month, any.day, any.hour, any.minute))
+                elif any == date_objects[4]:
+                    await self.bot.say(
+                                           "{} vs {} today, {}/{}- at {}:{} EST!".format(matchups[8], matchups[9],
+                                                                        any.month, any.day, any.hour, any.minute))
+                elif any == date_objects[5]:
+                    await self.bot.say(
+                                           "{} vs {} today, {}/{}- at {}:{} EST!".format(matchups[10], matchups[11],
+                                                                        any.month, any.day, any.hour, any.minute))
+                elif any == date_objects[6]:
+                    await self.bot.say(
+                                           "{} vs {} today, {}/{}- at {}:{} EST!".format(matchups[12], matchups[13],
+                                                                        any.month, any.day, any.hour, any.minute))
+                elif any == date_objects[7]:
+                    await self.bot.say(
+                                           "{} vs {} today, {}/{}- at {}:{} EST!".format(matchups[14], matchups[15],
+                                                                        any.month, any.day, any.hour, any.minute))
+                else:
+                    pass
+            else:
+                pass
 
 def setup(bot):
     bot.add_cog(Web(bot))
