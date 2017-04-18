4da2df9cc8d65216b518fa1019a8048b6d1f3d90
jwolfe <jgwolfe@udel.edu>
2017-04-17 23:25:02 -0400

    added docstrings, shortened lines

--- cogs/webcmds.py
+++ cogs/webcmds.py
@@ -1,24 +1,27 @@
-from discord.ext import commands
-import bs4 as bs
 from datetime import datetime
 import re
+from discord.ext import commands
+import bs4 as bs
 import pandas as pd
 import aiohttp
 
 http = aiohttp.ClientSession()
 
 class Web():
+    """Web holds all the commands relating to web-stuff.  WDL Standings and gametimes mostly."""
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def standings(self):
+        """!standings - returns current WDL season standings."""
         wdl_standings = pd.read_html("http://doomleague.org", index_col=0)
         standings_table = wdl_standings[0][["PTS", "PF", "PA"]]
         await self.bot.say("```WDL Season 7 Standings\n\n{}```".format(str(standings_table)))
 
     @commands.command()
     async def gametime(self):
+        """!gametime - returns a list of gametimes."""
         rege_str = r"Gametime:\s[\w]+,\s[\w]{3}\s[0-9]+\s@\s[0-9]+:[0-9][0-9]PM\sEST"
         playoff_gametime_re = r"Gametime:\s[\w]+,\s[\w]{3}\s[0-9]+\s@\s[0-9]+:[0-9][0-9]PM\sEDT"
 
@@ -34,6 +37,7 @@ class Web():
 
     @commands.command()
     async def today(self):
+        """!today - returns matchup and time if there is a game today."""
         #gametime regexs from wdl.org
         gametime_str = r"Gametime:\s[\w]+,\s[\w]{3}\s[0-9]+\s@\s[0-9]+:[0-9][0-9]PM\sEST"
         playoff_gametime_re = r"Gametime:\s[\w]+,\s[\w]{3}\s[0-9]+\s@\s[0-9]+:[0-9][0-9]PM\sEDT"
@@ -63,16 +67,26 @@ class Web():
 
         for obj in date_objects:
             if obj.day == tday.day and obj.month == tday.month:
-                await self.bot.say("**{} vs {}** - today {}/{} at {}:{} EST!".format(matchups[(date_objects.index(obj) * 2)],
-                                matchups[(date_objects.index(obj) * 2) + 1], obj.month, obj.day, obj.hour, obj.minute))
+                await self.bot.say("**{} vs {}** - today {}/{} at {}:{}"
+                                   "EST!".format(matchups[(date_objects.index(obj) * 2)],
+                                                 matchups[(date_objects.index(obj) * 2) + 1],
+                                                 obj.month,
+                                                 obj.day,
+                                                 obj.hour,
+                                                 obj.minute))
 
             else:
                 pass
 
         for obj in date_objects_playoffs:
             if obj.day == tday.day and obj.month == tday.month and tday.hour < obj.hour:
-                await self.bot.say("**{} @ {}** - today {}/{} at {}:{} EST!".format(playoff_matchups[(date_objects_playoffs.index(obj) * 2)],
-                                    playoff_matchups[(date_objects_playoffs.index(obj) * 2) + 1], obj.month, obj.day, obj.hour, obj.minute))
+                await self.bot.say("**{} @ {}** - today {}/{} at {}:{}"
+                                   "EST!".format(playoff_matchups[(date_objects_playoffs.index(obj) * 2)],
+                                                 playoff_matchups[(date_objects_playoffs.index(obj) * 2) + 1],
+                                                 obj.month,
+                                                 obj.day,
+                                                 obj.hour,
+                                                 obj.minute))
             else:
                 pass
 
