ba74a6a6dc439377e8e790d1f1e98e12b3116ee3
jwolfe <jgwolfe@udel.edu>
2017-12-30 14:56:09 -0500

    updated pickups.py

--- cogs/pickups.py
+++ cogs/pickups.py
@@ -22,7 +22,7 @@ class Pickups():
             while player_set:
                 player_set.pop()
         else:
-            await self.bot.say("**CTF**({}/{}) added.".format(len(player_set), players_for_priv))
+            await self.bot.say("**CTF**({}/{}) added".format(len(player_set), players_for_priv))
         await asyncio.sleep(3600)
         try:
             player_set.remove(ctx.message.author)
